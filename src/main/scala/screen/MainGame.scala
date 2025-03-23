7873d8f9b5b05f7b110ba0fff05728e70739bf3a
Brian Hsu <brianhsu.hsu@gmail.com>
2025-03-23 10:07:39 +0800

    EP04: Polling for handling keyboard.

--- src/main/scala/screen/MainGame.scala
+++ src/main/scala/screen/MainGame.scala
@@ -1,6 +1,7 @@
 package coding.games
 package screen
 
+import com.badlogic.gdx.Input.Keys
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.{Color, Texture}
 import com.badlogic.gdx.utils.ScreenUtils
@@ -14,6 +15,7 @@ class MainGame extends ApplicationListener {
   private lazy val bucketTexture: Texture = new Texture(Gdx.files.internal("bucket.png"))
 
   private var positionX: Float = 0.0f
+  private var positionY: Float = 0.0f
 
   override def create(): Unit = {}
 
@@ -21,13 +23,51 @@ class MainGame extends ApplicationListener {
 
   override def render(): Unit = {
 
+    val (offsetX, offsetY) = getOffsetFromUserInput()
+
+    updateGameWorld(offsetX, offsetY)
+
+    drawGameWorld()
+  }
+
+  private def updateGameWorld(offsetX: Float, offsetY: Float): Unit = {
+    this.positionX = Math.clamp(positionX + offsetX, 0, 1080 - bucketTexture.getWidth)
+    this.positionY = Math.clamp(positionY + offsetY, 0, 720 - bucketTexture.getHeight)
+  }
+
+  private def getOffsetFromUserInput(): (Float, Float) = {
+
+    println("getOffsetFromUserInput:" + System.currentTimeMillis())
+    var offsetX: Float = 0
+    var offsetY: Float = 0
+
+    val velocity = PIXEL_PER_SECOND * Gdx.graphics.getDeltaTime
+
+    if (Gdx.input.isKeyPressed(Keys.D)) {
+      offsetX = velocity
+    }
+
+    if (Gdx.input.isKeyPressed(Keys.A)) {
+      offsetX = - velocity
+    }
+
+    if (Gdx.input.isKeyPressed(Keys.W)) {
+      offsetY = velocity
+    }
+
+    if (Gdx.input.isKeyPressed(Keys.S)) {
+      offsetY = - velocity
+    }
+
+    (offsetX, offsetY)
+  }
+
+  private def drawGameWorld(): Unit = {
     ScreenUtils.clear(Color.DARK_GRAY)
 
     spriteBatch.begin()
-    spriteBatch.draw(bucketTexture, positionX, 0)
+    spriteBatch.draw(bucketTexture, positionX, positionY)
     spriteBatch.end()
-
-    positionX += PIXEL_PER_SECOND * Gdx.graphics.getDeltaTime
   }
 
   override def pause(): Unit = {}
